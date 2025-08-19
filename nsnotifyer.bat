f0687470527f156cf4f57e690ae1cd0c6354b6f5
Lorenzo Verheecke <32353997+VerheeckeLorenzo@users.noreply.github.com>
2025-08-19 14:29:09 +0200

    Update nsnotifyer.bat

--- nsnotifyer.bat
+++ nsnotifyer.bat
@@ -1,10 +1,10 @@
 @echo off
 
-set desired_ip=188.208.36.82
-set current_ip=176.62.173.233
+set desired_ip=0.0.0.0
+set current_ip=0.0.0.0
 
 :loop
-for /f "delims=\n " %%a in ('nslookup lorenzoverheecke.be ^| findstr "Addresses:"') do (
+for /f "delims=\n " %%a in ('nslookup yourdomainname.com ^| findstr "Addresses:"') do (
     set "line=%%b"
     set "line=!line:*Addresses: =!"
     set current_ip=!line!
